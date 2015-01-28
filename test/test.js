f5590e13cd7eeeb9815f2e133cb3ffa32e9b3e2d
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 17:16:53 +0100

    Various tweaks

--- test/test.js
+++ test/test.js
@@ -6,10 +6,10 @@ var readChunk = require('read-chunk');
 var test = require('ava');
 
 test('should detect BZIP2 from buffer', function (t) {
-    t.plan(2);
+	t.plan(2);
 
-    readChunk(path.join(__dirname, 'fixtures/test.bz2'), 0, 4, function (err, buf) {
-        t.assert(!err);
-        t.assert(isBzip2(buf));
-    });
+	readChunk(path.join(__dirname, 'fixtures/test.bz2'), 0, 4, function (err, buf) {
+		t.assert(!err, err);
+		t.assert(isBzip2(buf));
+	});
 });
